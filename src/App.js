f151332c6a94366a52ab5365e5846ab89a42d518
Simo Siren <sirensimo7@gmail.com>
2021-12-15 10:05:51 +0200

    sisennyksiä ym parannettu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, {useState, useEffect} from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Phone from './Phone'
 
 function App() {
 
-
 const [volunteers, setVolunteers] = useState([])
 const [idShowPhone, setIdShowPhone] = useState("")
-
 
 useEffect(() => {
   fetch("http://localhost:8000/api/volunteers")
@@ -21,18 +18,18 @@ useEffect(() => {
 
   return (
     <div className="App">
-     {
-       volunteers && volunteers.map(v => 
+        {
+          volunteers && volunteers.map(v => 
 
-          <div key={v.id}>
+              <div key={v.id}>
 
-            <h4>{v.name}</h4>
+                <h4>{v.name}</h4>
 
-            <Phone volunteer={v} idShowPhone={idShowPhone} />
+                <Phone volunteer={v} idShowPhone={idShowPhone} />
 
-          </div>
-        )
-     }
+              </div>
+            )
+        }
     </div>
   )
 }
--- src/Phone.jsx
+++ src/Phone.jsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function Phone(props) {
 
-if (props.volunteer.id === props.idToShowPhone)
-  return (
-    <p>{props.phone}</p>
-  )
+    if (props.volunteer.id === props.idToShowPhone)
+        return (
+            <p>{props.phone}</p>
+        )
 
-  else
-  return (
-    null
-  )
-
-
+    else {
+        return(null)
+    }
 
 }
 
