a5efc04fa8a382bedd04045830172ed72d3bbebe
Simo Siren <sirensimo7@gmail.com>
2021-12-15 10:30:09 +0200

    korjattu toimii

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import './App.css'
-import Phone from './Phone'
 
 function App() {
 
-const [volunteers, setVolunteers] = useState([])
-const [idShowPhone, setIdShowPhone] = useState("")
-
+const [volunteers, setVolunteers] = useState([{id:1, name: "Jannika", phone: "12312312"}, {id:2, name: "Anselmi", phone: "333444333"}])
+const [idShowPhone, setIdShowPhone] = useState(0)
+/*
 useEffect(() => {
   fetch("http://localhost:8000/api/volunteers")
   .then(res => res.json())
@@ -14,7 +13,10 @@ useEffect(() => {
 }
   , []
 )
+*/
 
+console.log(idShowPhone)
+console.log(volunteers)
 
   return (
     <div className="App">
@@ -23,9 +25,11 @@ useEffect(() => {
 
               <div key={v.id}>
 
-                <h4>{v.name}</h4>
+                <h4 onClick={() => setIdShowPhone(v.id)}> {v.name} </h4>
 
-                <Phone volunteer={v} idShowPhone={idShowPhone} />
+                {idShowPhone === v.id ? <p>{v.name}n puhelinnumero: {v.phone}</p> : null}
+
+                <p>_____________________________________________________________________</p>
 
               </div>
             )
